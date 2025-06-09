3ead002d5b47ddb5f2e465cd9ece9e9f9de088c9
joki <joki.romero@gmail.com>
2025-06-10 00:54:04 +0200

    Pruebas iniciales

--- centinela/config.py
+++ centinela/config.py
@@ -37,3 +37,4 @@ LOGO_ACTIVO = Image.open(os.path.join(carpeta, r"images\ojo_abierto.png"))
 LOGO_INACTIVO = Image.open(os.path.join(carpeta, r"images\ojo_cerrado.png"))
 ICONO_ACTIVO_FICH = os.path.join(carpeta, r"images\ojo_abierto.ico")
 
+TOKEN_TELEGRAM = "5822496306:AAFqwAXkBo_QTLhSdwi5OjCoG23fpxn2Ot0"
